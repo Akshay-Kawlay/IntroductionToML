264692b4be694b50d8019d506b7e2a8d5f68ebf8
Upneet Randhawa <upneet.randhawa@mail.utoronto.ca>
2019-04-03 16:10:47 -0400

    Update starter_gmm.py

--- a3/starter_gmm.py
+++ a3/starter_gmm.py
@@ -47,3 +47,48 @@ def log_posterior(log_PDF, log_pi):
     # log_post: N X K
 
     # TODO
+    
+def MoG(epochs, lr, K):
+    print(data.shape)
+    print(val_data.shape)
+    N = data.shape[0]
+    d = data.shape[1]
+    
+    #initialize centroids
+    MU = tf.get_variable(name="MU", shape=(K,d))
+    phi = tf.get_variable(tf.float32, shape=(K,1))#variable dimension of Kx1
+    psi = tf.get_variable(tf.float32, shape=(K,1))#variable dimension of Kx1
+    #print(MU)
+    #print(pi)
+    #print(variance)
+    
+    #initialize tensors and placeholders
+    X = tf.placeholder(tf.float32, shape=(N,d))#input placeholder dimension of Nxd
+    variance = tf.exp(phi)
+    log_GaussPDF_val = log_GaussPDF(X,MU,variance)
+    log_pi_val = logsoftmax(psi)
+    log_posterior_val = log_posterior(log_GaussPDF_val, log_pi_val)
+
+    loss = getLoss(log_posterior_val, tf.squeeze(log_pi_val))
+
+    adam_op = tf.train.AdamOptimizer(lr, beta1=0.9, beta2=0.99, epsilon=1e-5).minimize(loss)
+    
+    init = tf.global_variables_initializer()
+    
+    #initialize loss per iteration
+    loss_array = np.zeros((epochs,1))
+    
+    with tf.Session() as sess:
+        sess.run(init) 
+        i = 1
+        while(i < epochs):
+            sess.run(adam_op, feed_dict={X: data})
+            loss_array[i] = sess.run(loss, feed_dict={X: data})
+            print("Iteration: "+str(i)+" loss: "+str(loss_array[i]))
+            i+=1
+        #print(sess.run(pair_dist, feed_dict={X: data}))
+        # distribution = tf.argmin(pair_dist, axis=1)
+        # belongs_to = sess.run(distribution, feed_dict={X: data})
+        # K_percent = findKDistribution(belongs_to, K)
+        # print_scatter2Dplot(data, belongs_to, K)
+        #printLossCurve(loss_array)
